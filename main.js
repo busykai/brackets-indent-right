78bdac901c6bd05a7b0cabf02d7f1ff5f686c29c
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2014-01-20 01:11:56 -0300

    Working implementation.

--- main.js
+++ main.js
@@ -10,15 +10,21 @@ define(function (require, exports, module) {
         Document                = brackets.getModule("document/Document"),
         DocumentManager         = brackets.getModule("document/DocumentManager"),
         EditorManager           = brackets.getModule("editor/EditorManager"),
+        Strings                 = brackets.getModule("strings"),
         
-        /* Minimal number of lines to be sampled. Only code lines will be sampled. */
-        MIN_LINE_COUNT          = 20;
+        /* Number of lines to be sampled. Only code lines will be sampled. */
+        SAMPLE_LINES_NO         = 20,
+    
+        /* IDs of the status bar elements. */
+        INDENT_TYPE             = "indent-type",
+        INDENT_WIDTH            = "indent-width-input";
+        
+        
     /**
-     *
+     * Detects the indentation type used in the file. Samples MIn
      */
-        function sniff(document) {
-        var doc = document || DocumentManager.getCurrentDocument();
-            text = doc.getText(),
+    function sniff(doc) {
+        var text = doc.getText(),
             length = text.length,
             i = 0,
             line = 1;
@@ -63,8 +69,12 @@ define(function (require, exports, module) {
                     inLineComment = false;
                     lineNo++;
                     newLine = true;
-                    if (!inBlockComment && !inExpression && nestLevel) {
-                        samples++;
+                    if (spaceCount > 0) {
+                        spaceCount = 0;
+                    } else {
+                        if (!inBlockComment && !inExpression && nestLevel) {
+                            samples++;
+                        }
                     }
                     break;
                 case '{':
@@ -125,26 +135,60 @@ define(function (require, exports, module) {
                 spaceCount = 0;
             }
             i++;
-            if (i > length || samples > MIN_LINE_COUNT) {
+            if (i > length || samples > SAMPLE_LINES_NO) {
                 break;
             }
+        } /* while */
+        for (var k in map) {
+            if (map[k].samples > samples * 0.7) {
+                return map[k];
+            }
         }
+        return null;
     }
     
     /**
      * 
      */
     function set(indent) {
+        var $indentType,
+            $indentWidth;
         
+        $indentType     = $("#" + INDENT_TYPE);
+        $indentWidth    = $("#" + INDENT_WIDTH);
+        
+        if ($indentType.text() === Strings.STATUSBAR_SPACES) {
+            if (indent.char === '\t') {
+                $indentType.trigger("click");
+            }
+        } else if ($indentType.text() === Strings.STATUSBAR_TAB_SIZE) {
+            if (indent.char === ' ') {
+                $indentType.trigger("click");
+            }
+        }
+        
+        if (indent.char === ' ') {
+            $indentWidth.val(indent.indent);
+            $indentWidth.trigger("blur");
+        }
+        
+    }
+    
+    function run(input) {
+        var doc = input || DocumentManager.getCurrentDocument(),
+            indent;
+        if ((indent = sniff(doc))) {
+            set(indent);
+        }
     }
     
     AppInit.appReady(function () {
         $(DocumentManager)
-            .on("documentRefreshed.indent-right", function(e, doc) {
-                var indent;
-                if ((indent = sniff(doc))) {
-                    set(indent);
-                }
+            .on("documentRefreshed.indent-right documentSaved.indent-right", function(e, doc) {
+                run(doc);
+            })
+            .on("currentDocumentChange.indent-right", function(e) {
+                run();
             });
     });
 });
